3cb7829c1554772a48d91d2c58c118467a190f0c
dvlp-jun <lastcom1@gmail.com>
2022-09-21 18:25:11 +0900

    Q5622 다이얼
    다이얼 문제 C++
    정답&주석 완료

---
+++ 5622.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char c[16];
+	char p;
+	int time = 0;
+	cin >> c;
+	for(int i=0;i<strlen(c);i++){
+		p = c[i];
+		if(p>64&&p<68){
+			time +=3;
+		}
+		else if(p>67&&p<71){
+			time +=4;
+		}
+		else if(p>70&&p<74){
+			time +=5;
+		}
+		else if(p>73&&p<77){
+			time +=6;
+		}
+		else if(p>76&&p<80){
+			time +=7;
+		}
+		else if(p>79&&p<84){
+			time +=8;
+		}
+		else if(p>83&&p<87){
+			time +=9;
+		}
+		else if(p>86&&p<91){
+			time +=10;
+		}
+		else{
+			time+=11;
+		}
+	}
+	
+	cout << time << endl;
+	
+	return 0;
+} 
