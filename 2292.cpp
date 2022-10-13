aa803d7195cc683676775ee2fd7cc3a593656675
dvlp-jun <lastcom1@gmail.com>
2022-10-13 14:44:24 +0900

    Update 2292.cpp
    오타 수정

--- 2292.cpp
+++ 2292.cpp
@@ -7,9 +7,9 @@ int main(void) {
 	int count = 0;
 	
 	//띠가 2부터 시작하기 때문에 sum은 2부터 시작
-	for (int sum = 2; sum < x; count++) {
+	for (int sum = 2; sum <= x; count++) {
 		//벌집은 6각이므로 1을 기준으로 둘러싸는 벌집의 수는 6의 등비수열로 증가.
-		//그 수가 원하는 x 이내에 최대로 근접했을 때의 count 수가 이동하는 칸의 수. 
+		//그 수가 원하는 x에 최대로 근접했을 때의 count 수가 이동하는 칸의 수. 
 		sum += 6 * count;
 	}
 	
