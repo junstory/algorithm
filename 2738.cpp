6f77355c6cbf1205f7835581dcac26f3fabd823d
Junho <lastcom1@gmail.com>
2022-11-01 11:01:41 +0900

    Q.2738
    주석 추가

--- 2738.cpp
+++ 2738.cpp
@@ -6,9 +6,11 @@ int main(void) {
 	int n, m, temp;
 	cin >> n >> m;
 
+	//벡터를 이용한 2차원 배열 초기화.
 	vector<vector<int>> m1(n, vector<int>(m,0));
 	vector<vector<int>> m2(n, vector<int>(m,0));
 
+	//각 행렬 입력 받음.
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> temp;
@@ -22,6 +24,7 @@ int main(void) {
 		}
 	}
 
+	//더한 값을 그대로 출력.
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cout << m1[i][j]+m2[i][j] << ' ';
