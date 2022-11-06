9b85bc905161e80fe3b690ceb0e610382b612c39
Junho <lastcom1@gmail.com>
2022-11-06 21:03:41 +0900

    Q.10250
    ACM호텔 문제
    정답&주석풀이 완료

--- 10250.cpp
+++ 10250.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main(void) {
 	int t, h, w, n; //문제 내 입력받는 변수
+	//실행횟수
 	cin >> t;
+	//t만큼 실행
 	for (int k = 0; k < t; k++) {
 		cin >> h >> w >> n;
 		int count = 0;
-		for (int i = 1; i < w; i++) {
-			for (int j = 1; j < h; j++) {
+		//각층의 x01호, x02호,,,,순으로 채워지므로 
+		//for문을 h->w 순이아닌 w->h순으로 구성
+		//count가 n이 될 때까지 방을 채워나감.
+		for (int i = 1; i <= w; i++) {
+			for (int j = 1; j <= h; j++) {
 				count++;
 				if (count == n) {
 					if (i < 10) {
