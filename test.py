0fd3315dac991947016493a1b4f4764633edbe54
junstory <lastcom1@gmail.com>
2024-12-30 09:58:22 +0900

    Update gitignore

--- test.py
+++ test.py
@@ -1,26 +1,21 @@
 from collections import deque
 
-n = int(input())
-ret = []
-for i in range(n):
-    ret.append(int(input()))
-
-ans = []
-q = deque([])
-for i in range(1, n+1):
-    q.appendleft(i)
-    temp = q.popleft()
-    if temp == ret[0]:
-        while len(ret) != 0 and temp == ret[0]:
-            ans.append('-')
-            ret.pop(0)
-            if len(q) != 0:
-                temp = q.popleft()
-        q.appendleft(temp)
-    else:
-        q.appendleft(temp)
-        ans.append('+')
-print(ret)
-print(ans)
-print(q)
+def bfs(start, end):
     
+    queue = deque([(start, 0)])
+    visited = set([(start)])
+
+    while queue:
+        curr, time = queue.popleft()
+
+        if curr== end:
+            return time
+        
+        for next in (curr +1, curr -1, curr*2):
+            if 0 <= next <= 100000 and next not in visited:
+                queue.append((next, time+1))
+                visited.add(next)
+    return -1
+
+n, m = map(int,input().split())
+print(bfs(n,m))
