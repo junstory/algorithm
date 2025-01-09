e06c11d39276451f2b9e12f8288742d380ca1c9a
Junho Lee <lastcom1@gmail.com>
2025-01-09 11:49:11 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+
+def backtrack(curr_seq, start):
+    if len(curr_seq) == m:
+        print(' '.join(map(str,curr_seq)))
+        return
+    
+    for i in range(start, n+1):
+        if i not in curr_seq:
+            backtrack(curr_seq + [i], start)
+
+
+backtrack([], 1)
