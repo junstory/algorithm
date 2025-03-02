e5f4a1c98e408a790423e5ebed054a30f0eb9497
Junho Lee <lastcom1@gmail.com>
2025-03-03 00:52:14 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,22 @@
+-- 코드를 작성해주세요
+WITH RECURSIVE generations AS (
+    SELECT ID, PARENT_ID, 1 AS GENERATION
+    FROM ECOLI_DATA 
+    WHERE PARENT_ID IS NULL
+    
+    UNION ALL
+    
+    SELECT e.ID, e.PARENT_ID, g.GENERATION + 1
+    FROM ECOLI_DATA e
+    INNER JOIN generations g 
+    ON e.PARENT_ID = g.ID
+)
+
+SELECT COUNT(*) AS COUNT, G.GENERATION AS GENERATION
+    FROM generations g
+    WHERE NOT EXISTS (
+        SELECT 1 
+        FROM ECOLI_DATA child
+        WHERE child.PARENT_ID = g.ID)
+    GROUP BY GENERATION
+    ORDER BY GENERATION
